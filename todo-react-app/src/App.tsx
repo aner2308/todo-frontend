2ba4493c9b7a9711ce09cdd1fa94c68ed2a71822
Anton <erikssonanton94@gmail.com>
2026-02-10 21:18:13 +0100

    Skapat interface för mitt formulärdata. Har även skapat en useState som använder interfacet. Alla input-fält har en onChange som läser av förändringar och asssignar de nya värdena

--- todo-react-app/src/App.tsx
+++ todo-react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import TodoForm from './components/TodoForm'
 
 function App() {
   return (
     <>
      <h1>Test av applikation</h1>
+     <TodoForm />
     </>
   )
 }
--- todo-react-app/src/components/TodoForm.tsx
+++ todo-react-app/src/components/TodoForm.tsx
@@ -11,22 +11,31 @@ const TodoForm = () => {
 
     const [formData, setFormData] = useState<FormData>({ title: "", description: "", status: 0 })
 
+    const submitForm = ((event: any) => {
+        event?.preventDefault();
+
+        console.log("Test av funktion submit")
+    }) 
+
     return (
-        <form action="">
+        <form onSubmit={submitForm}>
             <label htmlFor="title">Titel</label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title" value={formData.title} 
+            onChange={(event) => setFormData({...formData, title: event.target.value})}/>
 
             <label htmlFor="description">Beskrivning</label>
-            <textarea name="description" id="description"></textarea>
+            <textarea name="description" id="description" value={formData.description}
+            onChange={(event) => setFormData({...formData, description: event.target.value})}></textarea>
 
             <label htmlFor="status">Status</label>
-            <select name="status" id="status">
+            <select name="status" id="status" value={formData.status}
+            onChange={(event) => setFormData({...formData, status: Number(event.target.value)})}>
                 <option value="0">Ej påbörjad</option>
                 <option value="1">Pågående</option>
                 <option value="2">Avklarad</option>
             </select>
 
-            <button type="submit">+ Spara till listan</button>
+            <button type="submit">Spara till listan</button>
         </form>
     )
 }
