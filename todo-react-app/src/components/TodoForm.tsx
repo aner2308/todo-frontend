be7c3adf780714736df61a5d160cdd366b04b0f6
Anton <erikssonanton94@gmail.com>
2026-02-11 17:43:13 +0100

    Lagt till validering och felmeddelanden i mitt formulär.

--- todo-react-app/src/components/TodoForm.tsx
+++ todo-react-app/src/components/TodoForm.tsx
@@ -9,12 +9,50 @@ const TodoForm = () => {
         status: number
     }
 
+    interface ErrorsData {
+        title?: string,
+        description?: string
+    }
+
+    //state för formulär
     const [formData, setFormData] = useState<FormData>({ title: "", description: "", status: 0 })
+    
+    //state för errors
+    const [errors, setErrors] = useState<ErrorsData>({})
+    
+    const validateForm =((data: FormData) => {
+
+        //Tomt objekt för att lagra eventuella felmeddelanden
+        const validationErrors: ErrorsData = {};
+
+        //Felmeddelande vid saknad title
+        if(!data.title || data.title.length < 3) {
+            validationErrors.title = "Din titel måste vara minst tre tecken lång."
+        }
+
+        //Felmeddelande vid för lång beskrivning
+        if(data.description.length > 200 ) {
+            validationErrors.description = "Din beskrivning får max vara 200 tecken lång."
+        }
+
+        return validationErrors;
+    })
 
     const submitForm = ((event: any) => {
         event?.preventDefault();
 
-        console.log("Test av funktion submit")
+        //Lägger in eventuella felmeddelanden i validationErrors
+        const validationErrors = validateForm(formData);
+
+        //Om felmeddelanden finns används setErrors
+        if(Object.keys(validationErrors).length > 0) {
+
+            setErrors(validationErrors);
+        } else {
+            
+            //Tömmer objekt
+            setErrors({});
+        }
     }) 
 
     return (
@@ -23,9 +61,13 @@ const TodoForm = () => {
             <input type="text" name="title" id="title" value={formData.title} 
             onChange={(event) => setFormData({...formData, title: event.target.value})}/>
 
+            {errors.title && <span className="errorMessage">{errors.title}</span>}
+
             <label htmlFor="description">Beskrivning</label>
             <textarea name="description" id="description" value={formData.description}
             onChange={(event) => setFormData({...formData, description: event.target.value})}></textarea>
+
+            {errors.description && <span className="errorMessage">{errors.description}</span>}
 
             <label htmlFor="status">Status</label>
             <select name="status" id="status" value={formData.status}
